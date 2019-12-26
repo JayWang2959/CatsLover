10ed8904cdea0d3024f49daf2add541078c8b882
JayWang <2643267669@qq.com>
2019-12-26 11:18:55 +0800

    完善登录注册的校验

--- CatsLover_manager/CatsLover_manager_web/src/main/java/com/CatsLover/controller/CatsLoverController.java
+++ CatsLover_manager/CatsLover_manager_web/src/main/java/com/CatsLover/controller/CatsLoverController.java
@@ -4,12 +4,13 @@ package com.CatsLover.controller;
 import com.CatsLover.pojo.CatsloverUser;
 import com.CatsLover.service.CatsLoverLoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+
 
 
 @Controller()
@@ -18,33 +19,29 @@ public class CatsLoverController {
     @Autowired
     CatsLoverLoginService catsLoverLoginService;
 
-    @RequestMapping("/user/{userid}")
-    @ResponseBody
-    public CatsloverUser getUserById(@PathVariable String userid) {
-        CatsloverUser user = catsLoverLoginService.getUserById(userid);
-        return user;
-    }
-
     @RequestMapping(value="login")
     public String logincheck(@RequestParam(required=false) String login_id, String login_password, Model model){
-        System.out.println(login_id);
-        System.out.println("1");
-        System.out.println(login_password);
-        System.out.println("2");
-        CatsloverUser user = catsLoverLoginService.getUserById(login_id);
-        System.out.println("2.5");
-        if(user == null)
-            System.out.println("shit");
-        System.out.println("3");
-        System.out.println(user.getUserUsername());
-        System.out.println(user.getUserPassword());
+        CatsloverUser user;
+
+        try {
+            user = catsLoverLoginService.getUserById(login_id);
+        }catch (Exception e) {
+//            System.out.println(e.getClass().getName());
+            if (e instanceof IndexOutOfBoundsException) {
+                model.addAttribute("error", "用户不存在！");
+                return "loginpage";
+            }
+            else{
+                model.addAttribute("error", "抱歉，出现未知错误，请再试一次");
+                return "loginpage";
+            }
+        }
+
         if(login_password.equals(user.getUserPassword())){
-            System.out.println("true");
             model.addAttribute("loginuser", "欢迎"+user.getUserUsername()+"!");
             return "homepage";
         }
         else{
-            System.out.println("false");
             model.addAttribute("error", "密码错误！");
             return "loginpage";
         }
@@ -53,15 +50,21 @@ public class CatsLoverController {
 
     @RequestMapping(value="register")
     public String submitUser(@RequestParam(required=false) String register_id,String register_username, String register_password, Model model){
-        System.out.println(register_id);
-        System.out.println("1");
-
-        System.out.println(register_username);
-        System.out.println("2");
-        System.out.println(register_password);
-        System.out.println("3");
-        catsLoverLoginService.submitInfo(register_id, register_username, register_password);
-        return null;
+        try {
+            catsLoverLoginService.submitInfo(register_id, register_username, register_password);
+        }catch (Exception e) {
+            if (e instanceof DuplicateKeyException) {
+                e.printStackTrace();
+                model.addAttribute("error", "用户id已存在！");
+                return "registerpage";
+            }
+            else{
+                model.addAttribute("error", "抱歉，出现未知错误，请再试一次");
+                return "registerpage";
+            }
+        }
+            model.addAttribute("loginuser", "欢迎"+register_username+"!");
+        return "homepage";
     }
 
 
