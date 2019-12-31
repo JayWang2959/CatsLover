87e30aa4d0c79a858da5ea5e12ac4a820544f941
phantom765 <501290809@qq.com>
2019-12-31 19:00:41 +0800

    vote12.31

--- CatsLover_manager/CatsLover_manager_web/src/main/java/com/CatsLover/controller/CatsLoverVoteController.java
+++ CatsLover_manager/CatsLover_manager_web/src/main/java/com/CatsLover/controller/CatsLoverVoteController.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-//import com.jspsmart.upload.*;
-//import java.io.*;
-//import java.awt.Image;
-//import java.awt.image.*;
+import com.jspsmart.upload.*;
+import java.io.*;
+import java.awt.Image;
+import java.awt.image.*;
+import java.util.Calendar;
 
 @Controller
 public class CatsLoverVoteController {
@@ -20,26 +21,26 @@ public class CatsLoverVoteController {
     @RequestMapping(value = "/vote")
     public String submitCat(@RequestParam(required = false) String cat_name, String user_id, String apply, String image, Integer vote, Model model) {
         try {
-//            String url = "upload/";
-//            String saveurl = "";
-//            SmartUpload smartupload = new SmartUpload();
+            String url = "upload/";
+            String saveurl = "";
+            SmartUpload smartupload = new SmartUpload();
 //            smartupload.initialize(pageContext);
-//            smartupload.upload();//上传
-//            int count = smartupload.save("/upload");//保存到文件夹upload
-//            com.jspsmart.upload.File myFile = smartupload.getFiles().getFile(0);
-//            String myFileName = myFile.getFileName();//获得图片名
-//            String ext = myFile.getFileExt(); //获得图片名的后缀
-//            int file_size = myFile.getSize(); //取得文件的大小
-//            Calendar calendar = Calendar.getInstance();
-//            String filename = String.valueOf(calendar.getTimeInMillis());
-//            saveurl = "F:/phantom/Xshell(1)/";
-//            saveurl += url;
-//            saveurl += filename + "." + ext;
-////        myFile.saveAs(saveurl, smartupload.SAVE_PHYSICAL);
-//            String upurl = "";
-//            int vote1 = 0;
-//            upurl += url;
-//            upurl += filename + "." + ext;
+            smartupload.upload();//上传
+            int count = smartupload.save("/upload");//保存到文件夹upload
+            com.jspsmart.upload.File myFile = smartupload.getFiles().getFile(0);
+            String myFileName = myFile.getFileName();//获得图片名
+            String ext = myFile.getFileExt(); //获得图片名的后缀
+            int file_size = myFile.getSize(); //取得文件的大小
+            Calendar calendar = Calendar.getInstance();
+            String filename = String.valueOf(calendar.getTimeInMillis());
+            saveurl = "F:/phantom/Xshell(1)/";
+            saveurl += url;
+            saveurl += filename + "." + ext;
+            myFile.saveAs(saveurl, smartupload.SAVE_PHYSICAL);
+            String upurl = "";
+            int vote1 = 0;
+            upurl += url;
+            upurl += filename + "." + ext;
             catsLoverVoteApply.submitRegInfo(cat_name, user_id, apply, image, vote);
 
         } catch (Exception e) {
@@ -49,7 +50,7 @@ public class CatsLoverVoteController {
                 return "votepage2";
             } else {
                 e.printStackTrace();
-                model.addAttribute("error", "抱歉，出现未weqeqwe知错误，请再试一次");
+                model.addAttribute("error", "抱歉，出现未知错误，请再试一次");
                 return "votepage2";
             }
         }
